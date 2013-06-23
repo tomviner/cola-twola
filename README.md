ae3f92c5e16532ffbf25c934441a8b76d55f2334
TomV <tom@viner.tv>
2013-06-23 18:26:43 +0100

    Initial high level tests. v0.0.1

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ cola-twola
 ==========
 
 A web app that displays the most positive of tweets about a certain fizzy drink
+
+
+Changelog
+---------
+
+v0.0.1:
+  Initial high level tests
---
+++ twola/tests.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from .models import import_tweets_from_json, load_tweets
+
+
+JSON_RESPONSES = [
+    # ids: 7, 13
+    """[{"created_at":"2012-09-27T16:14:27Z","followers":9,"id":7,"message":"Vimto or Ribena?  You decide!","sentiment":0.2,"updated_at":"2012-09-27T16:14:27Z","user_handle":"@mad4vimto"},{"created_at":"2012-09-27T16:18:09Z","followers":3,"id":13,"message":"I've got to say - Pepsi max is great!","sentiment":0.9,"updated_at":"2012-09-27T16:18:09Z","user_handle":"@tasteless"}]""",
+    # an error message, not to be stored in the db
+    """{"error":{"message":"Server no respondy"}}""",
+    # ids: 3 (contains coke keyword), 13 (dup of above)
+    """[{"created_at":"2012-09-27T16:11:15Z","followers":24,"id":3,"message":"Coke is it!","sentiment":1.0,"updated_at":"2012-09-27T16:11:15Z","user_handle":"@coke_snortr"},{"created_at":"2012-09-27T16:18:09Z","followers":3,"id":13,"message":"I've got to say - Pepsi max is great!","sentiment":0.9,"updated_at":"2012-09-27T16:18:09Z","user_handle":"@tasteless"}]"""
+]
+
+
+def mock_tweet_source():
+    for json_string in JSON_RESPONSES:
+        yield json_string
+
+
+def test_load_tweets():
+    """
+    Load some tweets and make sure they are persistent.
+    With server error resposes and duplicate tweets ignored.
+    """
+    import_tweets_from_json(json_string, tweet_source=mock_tweet_source)
+
+    tweets = load_tweets(just_coke=False)
+    expected_ids = set([3, 7, 13])
+    expected_num = len(expected_ids)
+    assert len(tweets) == expected_num, "Found %s, expected %s" % (len(tweets), expected_num)
+    found_ids = set(tweet.id for id in tweets)
+    assert found_ids == expected_ids, "Found %s, expected %s" % (found_ids, expected_ids)
+
+    tweets = load_tweets(just_coke=True)
+    expected_ids = set([3])
+    expected_num = len(expected_ids)
+    assert len(tweets) == expected_num, "Found %s, expected %s" % (len(tweets), expected_num)
+
+
